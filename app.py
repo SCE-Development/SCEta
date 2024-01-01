d60e8a82dd89e7bb0be3118c74acafecb6a7b106
steven <59713070+steeevin88@users.noreply.github.com>
2024-01-01 15:15:17 -0800

    add server + endpoints (#1)

---
+++ app.py
@@ -0,0 +1,126 @@
+import logging
+import sys
+import threading
+import time
+import typing
+from dataclasses import dataclass
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+import requests
+import uvicorn
+import yaml
+
+from args import get_args
+
+
+app = FastAPI()
+args = get_args()
+
+with open(args.config, "r") as stream:
+    try:
+        data = yaml.safe_load(stream)
+        API_KEY = data.get("api_key", None)
+        stops = data.get("stops", {})
+    except Exception:
+        logging.exception("unable to open yaml file/ file is missing data, exiting")
+        sys.exit(1)
+
+logging.Formatter.converter = time.gmtime
+
+formatter = logging.basicConfig(
+    format="%(asctime)s.%(msecs)03dZ %(threadName)s %(levelname)s:%(name)s:%(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=logging.INFO,
+)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@dataclass
+class Prediction:
+  route: str
+  times: typing.List[str]
+
+@dataclass
+class Stop:
+  id: str
+  name: str
+  predictions: typing.List[Prediction]
+
+PREDICTIONS_URL = 'https://api.511.org/transit/StopMonitoring'
+cache = []
+def update_cache():
+    # clear cache of any previous (now outdated) data
+    cache.clear()
+    # send post request for each agency's stop(s)
+    for stop in stops:
+        agency = stop.get('operator')
+        stopCode = stop.get('id')
+        stopName= stop.get('name')
+
+        params = {
+            'api_key': API_KEY,
+            'agency': agency,
+            'stopCode': stopCode,
+            'format': 'json'
+        }
+
+        response = requests.get(PREDICTIONS_URL, params=params)
+
+        if response.status_code != 200:
+            logging.error(f"not parsing response because response code was {response.status_code}")
+            sys.exit(1)
+
+        content = response.content.decode('utf-8-sig')
+        data = json.loads(content)
+
+        all_incoming_buses = data.get('ServiceDelivery', {}).get('StopMonitoringDelivery', {}).get('MonitoredStopVisit')
+        unique_buses = {}
+        for bus in all_incoming_buses:
+            route_name = bus.get('MonitoredVehicleJourney', {}).get('LineRef')
+            expected_arrival = bus.get('MonitoredVehicleJourney', {}).get('MonitoredCall', {}).get('AimedArrivalTime')
+            
+            if route_name not in unique_buses:
+                unique_buses[route_name] = []
+                
+            unique_buses[route_name].append(expected_arrival)
+
+        # for each unique bus, make a prediction object
+        predictions = []
+        for bus in unique_buses:
+            pred = Prediction(
+                bus,
+                unique_buses[bus]
+            )
+            predictions.append(pred)
+            
+        stopInfo = Stop(stopCode, stopName, predictions)
+        cache.append(stopInfo)
+        
+    return
+
+def helper_thread():
+    while True:
+        update_cache()
+        logging.debug("helper thread updated cache with predictions")
+        time.sleep(60*10)
+
+@app.get('/predictions')
+async def predictions():
+    return cache
+
+if __name__ == 'app':
+    helper = threading.Thread(target=helper_thread, daemon=True)
+    helper.start()
+
+if __name__ == '__main__':
+    logging.info(f"running on {args.host}, listening on port {args.port}")
+    uvicorn.run("app:app", host=args.host, port=args.port, reload=True)
---
+++ args.py
@@ -0,0 +1,8 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", help="yaml file", required=True)
+    parser.add_argument("--host", default="0.0.0.0", help="server's ip address, default is 0.0.0.0")
+    parser.add_argument("--port", default="8000", help="server's port number, default is 8000", type=int)
+    return parser.parse_args()
