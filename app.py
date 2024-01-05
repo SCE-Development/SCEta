d386294a24b8afa5c259404352704506519e71fa
Dung Dong <dongtandung2001@gmail.com>
2024-01-05 00:14:03 -0800

    log debug for api's res status code

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def update_cache():
 
         with MetricsHandler.api_latency.time():
             response = requests.get(PREDICTIONS_URL, params=params)
-
+        logging.debug(f'511`s API response code was {response.status_code}')
         # unsuccessful request to 511 API
         if response.status_code != 200:
             MetricsHandler.api_response_codes.labels(response.status_code).inc() 
