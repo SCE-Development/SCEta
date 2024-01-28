8fc63670f2a1d3c6331cbf738ad2a430b2af2b06
Steven Le <stevenleusa79@yahoo.com>
2024-01-27 21:28:42 -0800

    changed nginx to work

--- frontend/src/APIFunctions/SCEta.js
+++ frontend/src/APIFunctions/SCEta.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { ApiResponse } from './ApiResponses.js';
 
-let SCETA_API_URL = 'http://localhost:8001/predictions';
 
 export async function getTransitPredictions() {
+  const scetaUrl = new URL('api/predictions', window.location.href)
   let status = new ApiResponse();
   await axios
-    .get(SCETA_API_URL)
+    .get(scetaUrl.href)
     .then(res => {
       status.responseData = res.data;
     })
