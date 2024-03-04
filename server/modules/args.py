6a3347898ee82d744303ec0403090e65d6225194
Steven Le <stevenleusa79@yahoo.com>
2024-03-03 21:02:18 -0800

    argparse + docker-compose changes

--- server/modules/args.py
+++ server/modules/args.py
@@ -4,7 +4,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", help="yaml file", required=True)
-    parser.add_argument("--fixed_stops", help="json file with predetermined stop data")
+    parser.add_argument("--fixed-stops", help="json file with predetermined stop data")
     parser.add_argument("--host", default="0.0.0.0", help="server's ip address, default is 0.0.0.0")
     parser.add_argument("--port", default="8001", help="server's port number, default is 8001", type=int)
     parser.add_argument("--verbose", '-v', action='count', default=0, help="increase logging verbosity; can be used multiple times")
