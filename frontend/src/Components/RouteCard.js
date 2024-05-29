aa0661c2e18dfa628994ca61c63e2878ab1457bd
Trung Tran <48637790+trungtran1234@users.noreply.github.com>
2024-05-28 23:07:24 -0700

    implement map to show stop locations (#28)
    * added stop coordinates to API prediction response
    
    * Update server/app.py
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    * added missing coordinate
    
    * implemented map to show stop location
    
    * reformatted map and table
    
    * update rotuecard
    
    * changed route and destination name to separate rows
    
    * fixed route and time alignment
    
    * Update frontend/src/TransitPredictionsPage.js
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- frontend/src/Components/RouteCard.js
+++ frontend/src/Components/RouteCard.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { formatDateToTime } from '../util/formatDateToTime';
 
-export default function RouteCard({ route, destinations }) {
+export default function RouteCard({ route, destinations, useDestinationAsName }) {
   return (
     <div className="mb-4">
-      <b className="badge badge-lg badge-primary rounded-md h-[3rem] mb-1 text-white text-2xl md:text-3xl">{route}</b>
-      <div className="grid gap-x-2 grid-cols-[50%_50%]">
-        <div className="col-span-2 my-0 divider"/>
-        <span>DESTINATION</span>
-        <span>EXPECTED ARRIVALS</span>
-        <div className="col-span-2 m-0 divider"/>
+      <div className="flex flex-row justify-between items-center">
+        <div className="col-span-2 my-0 divider" />
+        <div className="col-span-2 m-0 divider" />
       </div>
-      {Object.entries(destinations).map(([destination, times]) => (
+      {Object.entries(destinations).map(([destination, times ]) => (
         <div key={destination}>
-          <div className="grid gap-2 grid-cols-[50%_50%] text-2xl">
-            <span>{destination}</span>
-            <span>
+          <div className="md:grid md:gap-2 xl:grid-cols-[60%_40%] md:grid-cols-[45%_55%] text-xl md:text-2xl md:w-[60vw] w-[70vw]">
+            <div>
+              <b className=" mb-1 text-primary text-[1rem] md:text-3xl">{route}</b>
+              {useDestinationAsName ? null : (
+                <span className="mb-1 text-white text-[1rem] md:text-3xl"> to {destination}</span>
+              )}
+            </div>
+            <span className="text-[1rem] md:text-2xl md:text-right">
               {times.map((time, timeIndex) => (
                 <span key={timeIndex}>
                   {formatDateToTime(time)} {timeIndex !== times.length - 1 && ', '}
                 </span>
               ))}
             </span>
-            <div className="col-span-2 m-0 divider"/>
+            <div className="col-span-2 m-0 divider" />
           </div>
         </div>
       ))}
--- frontend/src/TransitPredictionsPage.js
+++ frontend/src/TransitPredictionsPage.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { getTransitPredictions } from './APIFunctions/SCEta';
 import { formatDateToTime } from './util/formatDateToTime';
 import RouteCard from './Components/RouteCard';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 export default function TransitPredictionsPage() {
   const [error, setError] = useState();
@@ -31,10 +36,10 @@ export default function TransitPredictionsPage() {
   // for example "Santa Clara & 6th" becomes "santa-clara-and-6th"  
   function encode(stopName) {
     return stopName.replace(/\s+/g, '-')
-    .replace(/&/g, 'and')
-    .replace(/[^a-zA-Z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '')
-    .toLowerCase();
+      .replace(/&/g, 'and')
+      .replace(/[^a-zA-Z0-9]+/g, '-')
+      .replace(/^-+|-+$/g, '')
+      .toLowerCase();
   }
 
   // get stopOptions
@@ -61,6 +66,7 @@ export default function TransitPredictionsPage() {
           window.location.hash = firstStop
         }
       }
+
     }
     setStopOptions(Object.values(uniqueStops));
   }, [busData, selectedStop]);
@@ -69,6 +75,14 @@ export default function TransitPredictionsPage() {
     setSelectedStop(newStop);
     window.location.hash = encode(newStop)
   };
+
+  //set default marker icon
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+  });
 
   if (error) {
     return (
@@ -89,7 +103,7 @@ export default function TransitPredictionsPage() {
 
   return (
     <section className="bg-white dark:bg-gray-900 min-h-[calc(100vh)] ">
-      <div className="px-4 py-8 mx-auto lg:py-16 lg:px-6 max-w-7xl">
+      <div className="px-4 py-8 ml-5 mr-5 lg:py-16 lg:px-6 max-w-[100%]">
         <div className="mx-10 mb-8 text-center">
           <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 lg:text-4xl dark:text-white">SCEta's Transit Predictions</h2>
           <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Below predictions are up-to-date as of <span className='font-semibold'>{timeAtMount}</span></p>
@@ -97,7 +111,7 @@ export default function TransitPredictionsPage() {
         {/* Dropdown for smaller screens */}
         <div className="md:hidden flex flex-col justify-center space-x-4 overflow-x-auto">
           <select value={selectedStop} onChange={(e) => changeTab(e.target.value)}
-            className="px-4 py-2 text-xl font-semibold border-b-2 outline-none bg-gray-800">
+            className="px-4 py-2 text-sm md:text-xl font-semibold border-b-2 outline-none bg-gray-800">
             {stopOptions.map((stopName) => (
               <option key={stopName} value={stopName}>
                 {stopName}
@@ -106,38 +120,55 @@ export default function TransitPredictionsPage() {
           </select>
         </div>
         {/* Tabs for larger screens */}
-        <div className="hidden md:flex flex-row justify-center space-x-4 overflow-x-auto">
+        <div className="hidden items-center xl:flex flex-row justify-center space-x-4 overflow-x-auto">
           {stopOptions.map((stopName) => (
             <button key={stopName} className={`px-4 py-2 text-xl font-semibold border-b-2 
             ${selectedStop === stopName ? 'border-blue-500 text-blue-500' : 'border-transparent text-gray-500 hover:border-gray-300'}`}
-            onClick={() => changeTab(stopName)}>
+              onClick={() => changeTab(stopName)}>
               {stopName}
             </button>
           ))}
         </div>
-        <div>
+        <div className="flex flex-col xl:grid xl:grid-cols-[70%_30%] gap-8">
           {!!busData.length && busData.map((stop) => (
             stop.name === selectedStop &&
-              <div key={stop.name} className="flex flex-col mt-4 p-6 min-w-80 max-h-[55vh] text-xl overflow-y-auto
+            <div key={stop.name} className="items-center text-center md:text-left justify-center flex flex-col w-full xl:w-full mt-4 p-2 min-w-50 max-h-[55vh] text-xl
                 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <div className="font-bold text-4xl mb-10">{stop.name}</div>
-                {stop.predictions.length === 0 ? (
-                  <span className="text-2xl">No predictions available at this time</span>
-                ) : (
-                  <div>
-                    {stop.predictions
-                      .sort((a, b) => a.route.localeCompare(b.route))
-                      .map((prediction, predictionIndex) => (
-                        <RouteCard
-                          key={predictionIndex}
-                          route={prediction.route}
-                          destinations={prediction.destinations}
-                        />
-                      ))}
-                  </div>
-                )}
-              </div>
+              <div className="flex justify-center xl:justify-start font-bold text-[1rem] md:text-4xl mb-5 xl:mb-10">{stop.name}</div>
+              {stop.predictions.length === 0 ? (
+                <span className="text-2xl">No predictions available at this time</span>
+              ) : (
+                <div>
+                  {stop.predictions
+                    .sort((a, b) => a.route.localeCompare(b.route))
+                    .map((prediction, predictionIndex) => (
+                      <RouteCard
+                        key={predictionIndex}
+                        route={prediction.route}
+                        destinations={prediction.destinations}
+                        useDestinationAsName={stop.use_destination_as_name}
+                        timeAtMount={timeAtMount}
+                        /> 
+                    ))}
+                </div>
+              )}
+            </div>
           ))}
+          {!!busData.length && busData.map((stop) => (
+            stop.name === selectedStop &&
+            <div key={stop.name} className="overflow-visible xl:mr-10 mt-4 items-center">
+              <MapContainer center={[stop.latitude, stop.longitude]} zoom={16} className="mx-auto rounded-lg h-[30vh] xl:h-[29em] w-full">
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution="&copy; OpenStreetMap contributors"
+                />
+                <Marker position={[stop.latitude, stop.longitude]}>
+                  <Popup>{stop.name}</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          ))}
+
         </div>
       </div>
     </section>
