278b4435dbcc7e72ee0cf0283fb12704eeb6556e
Matt E <mathew.estrada@sjsu.edu>
2024-06-29 21:47:59 -0700

    PR: Call function implemented for stop IDs

--- frontend/src/TransitPredictionsPage.js
+++ frontend/src/TransitPredictionsPage.js
@@ -29,11 +29,6 @@ export default function TransitPredictionsPage() {
     getSCEtaPredictions();
   }, []);
 
-  // this does three things
-  // 1. replace all non alpha numeric chars in the stop name with dashes
-  // 2. replace the character "&" with "and" if it appears
-  // 3. convert all characters in the stop to lowercase.
-  // for example "Santa Clara & 6th" becomes "santa-clara-and-6th"  
   function encode(stopName) {
     return stopName.replace(/\s+/g, '-')
       .replace(/&/g, 'and')
@@ -42,38 +37,34 @@ export default function TransitPredictionsPage() {
       .toLowerCase();
   }
 
-  // get stopOptions
   useEffect(() => {
     let uniqueStops = {};
     if (busData) {
-      // get unique stop options
       busData.forEach((stop) => {
         let encodedHash = encode(stop.name);
-        if (!uniqueStops.hasOwnProperty(encodedHash)) {
-          uniqueStops[encodedHash] = stop.name;
-        }
+        uniqueStops[encodedHash] = {
+          name: stop.name,
+          ids: Array.isArray(stop.ids) ? stop.ids : [], 
+        };
       });
 
-      // if URL hash exists, set the tab to the encoded stop
-      // otherwise, set tab to the first unique stop + add URL hash
       if (!selectedStop && Object.keys(uniqueStops).length > 0) {
-        let hash = window.location.hash.replace(/^#/, '')
+        let hash = window.location.hash.replace(/^#/, '');
         if (uniqueStops.hasOwnProperty(hash)) {
-          setSelectedStop(uniqueStops[hash])
+          setSelectedStop(uniqueStops[hash].name);
         } else {
           let firstStop = Object.keys(uniqueStops)[0];
-          setSelectedStop(uniqueStops[firstStop]);
-          window.location.hash = firstStop
+          setSelectedStop(uniqueStops[firstStop].name);
+          window.location.hash = firstStop;
         }
       }
-
     }
     setStopOptions(Object.values(uniqueStops));
   }, [busData, selectedStop]);
 
   const changeTab = (newStop) => {
     setSelectedStop(newStop);
-    window.location.hash = encode(newStop)
+    window.location.hash = encode(newStop);
   };
 
   function maybeRenderCallButton(stopId) {
@@ -91,7 +82,6 @@ export default function TransitPredictionsPage() {
       </div>
     );
   }
-
   //set default marker icon
   delete L.Icon.Default.prototype._getIconUrl;
   L.Icon.Default.mergeOptions({
@@ -124,33 +114,55 @@ export default function TransitPredictionsPage() {
           <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 lg:text-4xl dark:text-white">SCEta's Transit Predictions</h2>
           <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Below predictions are up-to-date as of <span className='font-semibold'>{timeAtMount}</span></p>
         </div>
-        {/* Dropdown for smaller screens */}
         <div className="xl:hidden flex flex-col justify-center space-x-4 overflow-x-auto">
           <select value={selectedStop} onChange={(e) => changeTab(e.target.value)}
             className="px-4 py-2 text-sm md:text-xl font-semibold border-b-2 outline-none bg-gray-800">
-            {stopOptions.map((stopName) => (
-              <option key={stopName} value={stopName}>
-                {stopName}
+            {stopOptions.map((stop) => (
+              <option key={stop.name} value={stop.name}>
+                {`${stop.name} (${stop.ids.length > 0 ? stop.ids.join(', ') : 'No IDs'})`}
               </option>
             ))}
           </select>
         </div>
-        {/* Tabs for larger screens */}
+
+        {/* Dropdown for smaller screens */}
         <div className="hidden items-center xl:flex flex-row justify-center space-x-4 overflow-x-auto">
-          {stopOptions.map((stopName) => (
-            <button key={stopName} className={`px-4 py-2 text-xl font-semibold border-b-2 
-            ${selectedStop === stopName ? 'border-blue-500 text-blue-500' : 'border-transparent text-gray-500 hover:border-gray-300'}`}
-              onClick={() => changeTab(stopName)}>
-              {stopName}
+          {stopOptions.map((stop) => (
+            <button key={stop.name} className={`px-4 py-2 text-xl font-semibold border-b-2 
+            ${selectedStop === stop.name ? 'border-blue-500 text-blue-500' : 'border-transparent text-gray-500 hover:border-gray-300'}`}
+              onClick={() => changeTab(stop.name)}>
+              {`${stop.name} `}
             </button>
           ))}
         </div>
+
+        {/* Tabs for larger screens */}
         <div className="flex flex-col xl:grid xl:grid-cols-[70%_30%] gap-8">
           {!!busData.length && busData.map((stop) => (
             stop.name === selectedStop &&
-            <div key={stop.name} className="items-center text-center md:text-left flex flex-col w-full xl:w-full mt-2 p-6 min-w-50 max-h-[65vh] text-xl
-                bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 overflow-y-auto">
-              <div className="flex justify-center xl:justify-start font-bold text-[1rem] md:text-4xl mb-5 xl:mb-10">{stop.name}</div>
+            <div key={stop.name} className="items-center text-center md:text-left flex flex-col w-full xl:w-full mt-2 p-6 min-w-50 max-h-[65vh] text-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 overflow-y-auto">
+              
+              <div className="flex justify-center xl:justify-start font-bold text-[1rem] md:text-4xl mb-2 xl:mb-5">
+                {stop.name}
+              </div>
+              <div className="flex justify-center xl:justify-start text-sm md:text-lg text-gray-500 mb-4 xl:mb-6">
+                {stop.ids.length > 0 ? (
+                  <>
+                    {stop.ids.some(id => !isNaN(id)) ? (
+                      <a
+                        href={`tel:511p1p1,,${stop.ids.find(id => !isNaN(id))}`}
+                        className="inline-block px-2 py-1 ml-2 text-white bg-blue-600 border border-blue-600 rounded hover:bg-blue-700"
+                      >
+                        CLICK TO CALL
+                      </a>
+                    ) : (
+                      <span className="inline-block px-2 py-1 ml-2 text-gray-500 border border-gray-500 rounded">NO CALL AVAILABLE</span>
+                    )}
+                  </>
+                ) : (
+                  <span>No IDs</span>
+                )}
+              </div>
               {stop.predictions.length === 0 ? (
                 <span className="text-2xl">No predictions available at this time</span>
               ) : (
@@ -165,11 +177,13 @@ export default function TransitPredictionsPage() {
                         destinations={prediction.destinations}
                         useDestinationAsName={stop.use_destination_as_name}
                         timeAtMount={timeAtMount}
-                        /> 
+                      />
                     ))}
                 </div>
               )}
             </div>
+
+
           ))}
           {!!busData.length && busData.map((stop) => (
             stop.name === selectedStop &&
@@ -185,7 +199,6 @@ export default function TransitPredictionsPage() {
               </MapContainer>
             </div>
           ))}
-
         </div>
       </div>
     </section>
